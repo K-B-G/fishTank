b5335f36069df0b347202573ae1a40d27020ebba
K-B-G <rocker409@cfl.rr.com>
2023-07-26 16:12:58 -0400

    Making a commit incase I need to revert. Going to try to use the new this thing in more of the code than document selector.

--- my_code/my_jscript.js
+++ my_code/my_jscript.js
@@ -1,153 +1,142 @@
 "use strict";
 
-//const anime = require("animejs");
-
 $(document).ready(function () {
 	$(".button-collapse").sideNav();
 	$('select').material_select();
 });
+/*const once = {
+	once: true 
+};*/
 //WORK ON THIS LATER 
-document.querySelector("[name='feesh']").addEventListener("click", fishSwim);
-
 
 //NOTE: I like to use pascal case for global objects that contain helper functions or variables as a reminder that thet are global.
 //GLOBAL HELPER FUNCTIONS 
-var GlobeHlp = {
+var GLOBHLP = {
 	getSum: function (total, num) {
 		return total + num;
 	}
-};
+}
 
+//WORK ON LATER IT'S TO ADD NEW FISH ELEMENTS PROPPER LOOK INTO DOCUMENT FRAGMENTS
+let addFish = function (){
+   const nufish = '<div class="fish z_4"> <img class="angle betta_fish" src="img/vector_fighting_fish_by_stu_artgraphics-d533k02.png" alt="" height="479" width="511">';
+   //let fishDiv = document.createElement('div');
+	 document.querySelector('.veryback').insertAdjacentHTML("afterend", nufish);//.setAttribute('class', 'fish z_4');
+   //document.getElementById('tank').appendChild(fishDiv).setAttribute('class', 'fish z_4').bind(fishSwim);
+   let fishy = document.querySelector('.fish');
+   fishSwim.bind(fishy)();
+}
+document.querySelector("[name='feesh']").addEventListener("click", addFish);
 //This holds the fishes properties to manipulate and the functions to move it around
-function fishSwim() {
-
-	let onkneemay;
-
-	let fishProp = {
-		topP: 0,
-		leftP: 0,
-		nuTop: 0,
-		nuLeft: 0,
-		swimSpeed: 0,
-		counter: [0],
-		zCounter: 0,
-	};
-	//This is what moves the fish to a new posistion
-	function makeNewPosition() {
-
-		// Get viewport dimensions (remove the dimension of the div)
-		//do some stuff to get the height and width of TANK
-		//Then have something that subtracks the size value of FISH, NAV, and FOOTER from computed TANK diminsions
-		//var bounds_h = nav_h + fishImgHght + tank_h + gravel_h + footer_h + 
-		let topP = document.querySelector('.fish').computedStyleMap().get('transform')[1].y.value;
-		let leftP = document.querySelector('.fish').computedStyleMap().get('transform')[0].x.value;
-		let fishImgHght = document.querySelector('.angle').offsetHeight;
-		let fishImgWdth = document.querySelector('.angle').offsetWidth;
-		let tankHght = document.querySelector('#tank').offsetHeight - (fishImgHght - 20);
-		let tankWdth = document.querySelector('#tank').offsetWidth; - (fishImgWdth - 20);
-		let nuTop = Math.floor(Math.random() * tankHght);
-		let nuLeft = Math.floor(Math.random() * tankWdth);
-		let swimSpeed = Math.floor(Math.random() * 5000 + 3000);
-		let zCounter = Math.floor((Math.random() * 2) + 1);
-
-
-		return fishProp['topP'] = topP, fishProp['leftP'] = leftP, fishProp['nuTop'] = nuTop, fishProp['nuLeft'] = nuLeft, fishProp['swimSpeed'] = swimSpeed, fishProp.counter[1], fishProp['zCounter'] = zCounter;
-	}
-	makeNewPosition();
-	//test_position.push(newq[0], newq[1]);
-	//var zCounter = Math.floor((Math.random() * 2) + 1);
-	// Pushes 3rd array value to COUNTER ARRAY
-	//counter.push(newq[3]);
-
-	/* Start of ENHANCED FISH ANIMATION */
-
-	//NOTE if top number is low, that means it's high
-	//	IF Left numbers is low, that means it's lefter
-
-	if (fishProp.counter.reduce(GlobeHlp.getSum) != 1) {
-
-		// if it is going down AND right
-		if (fishProp.topP < fishProp.nuTop && fishProp.leftP < fishProp.nuLeft) {
-
-			document.querySelector('.angle').classList.remove('y_180_rotate', 'y_180_rotate_down', 'y_180_rotate_up', 'upward');
-
-			document.querySelector('.angle').classList.add('downward');
-		}
-
-		//if it is going up AND right
-		else if (fishProp.topP > fishProp.nuTop && fishProp.leftP < fishProp.nuLeft) {
-			document.querySelector('.angle').classList.remove('y_180_rotate', 'y_180_rotate_down', 'y_180_rotate_up', 'downward');
-			document.querySelector('.angle').classList.add('upward');
-			}
-
-		//if it is going up AND left
-		else if (fishProp.topP > fishProp.nuTop && fishProp.leftP > fishProp.nuLeft) {
-			document.querySelector('.angle').classList.remove('y_180_rotate', 'y_180_rotate_down', 'downward', 'upward');
-			document.querySelector('.angle').classList.add('y_180_rotate_up');
-
-		}
-
-		//if it is going down AND left
-		else if (fishProp.topP < fishProp.nuTop && fishProp.leftP > fishProp.nuLeft) {
-			document.querySelector('.angle').classList.remove('y_180_rotate', 'y_180_rotate_up', 'upward', 'downward');
-			document.querySelector('.angle').classList.add('y_180_rotate_down');
-		}
-
-		else {
-
-			//its all good man
-		}
-	}
-
-
-	// Adjusts the Z-INDEX on FISH
-	if (fishProp.zCounter == 1) {
-		if (document.querySelector('.fish').classList.contains('z_4')) {
-			document.querySelector('.fish').classList.remove('z_4');
-			document.querySelector('.fish').classList.add('z_2');
-		}
-		else {
-			//do nothing
-		}
-	}
-	else {
-		if (document.querySelector('.fish').classList.contains('z_2')) {
-			document.querySelector('.fish').classList.remove('z_2');
-			document.querySelector('.fish').classList.add('z_4');
-		}
-		else {
-			//do nothing
-		}
-	}
-	
-	/*$('.fish').animate({ top: fishProp.nuTop, left: fishProp.nuLeft }, fishProp.swimSpeed, function () {
-
-		//alert(test_position.valueOf());
-		fishSwim();
-
-	});*/
-	onkneemay = anime({
-		targets: '.fish',
-		translateX: fishProp.nuLeft,
-		translateY: fishProp.nuTop,
-		duration: fishProp.swimSpeed,
-		direction: 'normal',
-		delay: 100,
-		easing: 'easeOutQuart',
-		complete: function (){
-			//console.log(fishProp.topP);
-			fishSwim();
+	let fishSwim = function() {
+		
+		let onkneemay;//declaringthis one for later use
+		//this holds the current and newly made properties to move the fish
+		let fishProp = {
+			topP: 0,
+			leftP: 0,
+			nuTop: 0,
+			nuLeft: 0,
+			swimSpeed: 0,
+			counter: [0],
+			zCounter: 0,
+			fishy: this,
+		};
+		//This is what makes the new values to move the fish
+		let makeNewPosition = function() {
 			
-		}
-	});
-	
+			let topP = document.querySelector('.fish').computedStyleMap().get('transform')[1].y.value;
+			let leftP = document.querySelector('.fish').computedStyleMap().get('transform')[0].x.value;
+			let fishImgHght = document.querySelector('.angle').offsetHeight;
+			let fishImgWdth = document.querySelector('.angle').offsetWidth;
+			let tankHght = document.querySelector('#tank').offsetHeight - (fishImgHght - 20);
+			let tankWdth = document.querySelector('#tank').offsetWidth; - (fishImgWdth - 20);
+			let nuTop = Math.floor(Math.random() * tankHght);
+			let nuLeft = Math.floor(Math.random() * tankWdth);
+			let swimSpeed = Math.floor(Math.random() * 5000 + 3000);
+			let zCounter = Math.floor((Math.random() * 2) + 1);
 
+
+			return fishProp.topP = topP, fishProp.leftP = leftP, fishProp.nuTop = nuTop, fishProp.nuLeft = nuLeft, fishProp.swimSpeed = swimSpeed, fishProp.counter[1], fishProp.zCounter = zCounter;
+		}
+		makeNewPosition();
+//NOTE: if top number is low, that means it's high
+//	IF Left numbers is low, that means it's lefter
+
+//This should probably be a while or do while just ignore the cringe
+if (fishProp.counter.reduce(GLOBHLP.getSum) != 1) {
+
+	// if it is going down AND right
+	if (fishProp.topP < fishProp.nuTop && fishProp.leftP < fishProp.nuLeft) {
+
+		document.querySelector('.angle').classList.remove('y_180_rotate', 'y_180_rotate_down', 'y_180_rotate_up', 'upward');
+
+		document.querySelector('.angle').classList.add('downward');
+	}
+
+	//if it is going up AND right
+	else if (fishProp.topP > fishProp.nuTop && fishProp.leftP < fishProp.nuLeft) {
+		document.querySelector('.angle').classList.remove('y_180_rotate', 'y_180_rotate_down', 'y_180_rotate_up', 'downward');
+		document.querySelector('.angle').classList.add('upward');
+	}
+
+	//if it is going up AND left
+	else if (fishProp.topP > fishProp.nuTop && fishProp.leftP > fishProp.nuLeft) {
+		document.querySelector('.angle').classList.remove('y_180_rotate', 'y_180_rotate_down', 'downward', 'upward');
+		document.querySelector('.angle').classList.add('y_180_rotate_up');
+
+	}
+
+	//if it is going down AND left
+	else if (fishProp.topP < fishProp.nuTop && fishProp.leftP > fishProp.nuLeft) {
+		document.querySelector('.angle').classList.remove('y_180_rotate', 'y_180_rotate_up', 'upward', 'downward');
+		document.querySelector('.angle').classList.add('y_180_rotate_down');
+	}
+
+	else {
+
+		//its all good man
+	}
 }
 
 
-/*function haveFish(){
-	$('.background').prepend('<img id="theImg" src="http://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg" />')
-};*/
+// Adjusts the Z-INDEX on FISH
+if (fishProp.zCounter == 1) {
+	if (document.querySelector('.fish').classList.contains('z_4')) {
+		document.querySelector('.fish').classList.remove('z_4');
+		document.querySelector('.fish').classList.add('z_2');
+	}
+	else {
+		//do nothing
+	}
+}
+else {
+	if (document.querySelector('.fish').classList.contains('z_2')) {
+		document.querySelector('.fish').classList.remove('z_2');
+		document.querySelector('.fish').classList.add('z_4');
+	}
+	else {
+		//do nothing
+	}
+}
+
+onkneemay = anime({
+	targets: fishProp.fishy,
+	translateX: fishProp.nuLeft,
+	translateY: fishProp.nuTop,
+	duration: fishProp.swimSpeed,
+	direction: 'normal',
+	delay: 100,
+	easing: 'easeOutQuart',
+	complete: function () {
+		console.log(fishProp.nuTop);//How I was seeing the fucntion is still being called multiple times depending on how many times I clicked the button
+		fishSwim.bind(fishProp.fishy)();
+		}
+	});
+	
+}
+
+
 
 /*$('#btn1').click(function(){
 alert('Handler for .click() called.');
