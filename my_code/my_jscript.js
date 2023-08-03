90ed0f8a372400328f30fe7f620b621d4c7ce446
K-B-G <rocker409@cfl.rr.com>
2023-08-03 12:31:16 -0400

    Add files via upload
    Long story short, using git in a root sudo environment cause it to get borked. This is the fixed files. I fixed my dev environment to avoid this issue again. This is the commit that was supposed to be after the optimizeFishswim branch was done.

--- my_code/my_jscript.js
+++ my_code/my_jscript.js
@@ -3,228 +3,20 @@
 $(document).ready(function () {
 	$(".button-collapse").sideNav();
 	$('select').material_select();
-	fishSwim();
-
 });
+/*const once = {
+	once: true 
+};*/
+//WORK ON THIS LATER 
+
+//NOTE: I like to use pascal case for global objects that contain helper functions or variables as a reminder that thet are global.
 //GLOBAL HELPER FUNCTIONS 
-function getSum(total, num) {
-	return total + num;
-}
-/*function makeNewPosition(){
- 
-// Get viewport dimensions (remove the dimension of the div)
-var h = $(window).height() - 150;
-var w = $(window).width() - 150;
- 
-var nh = Math.floor(Math.random() * h);
-var nw = Math.floor(Math.random() * w);
- 
-return [nh,nw];    
- 
+var GLOBHLP = {
+	getSum: function (total, num) {
+		return total + num;
+	}
 }
 
-<<<<<<< HEAD
-function animateDiv(){
-var newq = makeNewPosition();
-$('#fish').animate({ top: newq[0], left: newq[1] }, 5000, function(){
-  animateDiv();        
-});
- 
-};*/
-
-var counter = [];
-var test_position = [];
-//This will mostly stay the same, just converting Jquery to vanilla ES6
-function fishSwim() {	
-
-function makeNewPosition() {
-
-	// Get viewport dimensions (remove the dimension of the div)
-	//do some stuff to get the height and width of TANK
-	//Then have something that subtracks the size value of FISH, NAV, and FOOTER from computed TANK diminsions
-	//var nav_h = $nav.height();
-	//var nav_w =  $nav.width();
-	//var fish_h = $('#fish').height();
-	//var fish_w = $('#fish").width();
-	//var tank_h = $('#tank').height();
-	//var tank_w = $('#tank').width();
-	//var gravel_h = $('#gravel').height();
-	//var gravel_w = $('#gravel').width();
-	//var footer_h = $('#footer').height();
-	//var footer_w = $('#footer').width();
-	//var bounds_h = nav_h + fish_h + tank_h + gravel_h + footer_h + 
-	var fish_h = document.querySelector('.fish > img').offsetHeight;
-	var fish_w = document.querySelector('.fish > img').offsetWidth;
-	var h = document.querySelector('#tank').offsetHeight - (fish_h - 20);
-	var w = document.querySelector('#tank').offsetWidth; - (fish_w - 20);
-	/*var fish = 2;
-	for(i=0; i< fish; i++){}*/
-
-	var nh = Math.floor(Math.random() * h);
-	var nw = Math.floor(Math.random() * w);
-	var swim_speed = Math.floor(Math.random() * (8000 - 3000) + 3000);
-
-	//alert(fish_w);
-	return [nh, nw, swim_speed, 1];
-
-}
-
-
-	
-
-
-
-
-	var newq = makeNewPosition();
-	test_position.push(newq[0], newq[1]);
-	var z_counter = Math.floor((Math.random() * 2) + 1);
-	// Pushes 3rd array value to COUNTER ARRAY
-	counter.push(newq[3]);
-
-	/* Start of ENHANCED FISH ANIMATION */
-
-	//NOTE if top number is low, that means it's high
-	//	IF Left numbers is low, that means it's lefter
-
-	//$('.fish').hasClass('.right_forty_down')
-
-	if (counter.reduce(getSum) != 1) {
-
-		// if it is going down AND right
-		if (test_position[0] < test_position[2] && test_position[1] < test_position[3]) {
-
-			if (document.querySelector('.fish > img').classList.contains('display_n')) {
-				document.querySelector('.fish > img').classList.remove('display_n');
-				document.querySelector('.fish > img + img').classList.add('display_n');
-			}
-			else {
-				//do nothing
-			}
-			//turn the fish down
-			document.querySelector('.testing').classList.remove('right_forty_up');
-			document.querySelector('.testing').classList.add('right_forty_down');
-		}
-
-		//if it is going up AND right
-		else if (test_position[0] > test_position[2] && test_position[1] < test_position[3]) {
-			// when the right facing fish is off, turn it on
-			if (document.querySelector('.fish > img').classList.contains('display_n')) {
-				document.querySelector('.fish > img').classList.remove('display_n');
-				document.querySelector('.fish > img + img').classList.add('display_n');
-			}
-			else {
-				//do nothing
-			}
-
-			// turn the fish up	
-			document.querySelector('.testing').classList.remove('right_forty_down');
-			document.querySelector('.testing').classList.add('right_forty_up');
-
-		}
-
-		//if it is going up AND left
-		else if (test_position[0] > test_position[2] && test_position[1] > test_position[3]) {
-			// when the left facing fish is off, turn it on
-			if (document.querySelector('.fish > img + img').classList.contains('display_n')) {
-				document.querySelector('.fish > img + img').classList.remove('display_n');
-				document.querySelector('.fish > img').classList.add('display_n');
-			}
-			else {
-				//do nothing
-			}
-
-			// turn the fish down	
-			document.querySelector('.testing').classList.remove('right_forty_up');
-			document.querySelector('.testing').classList.add('right_forty_down');
-
-		}
-
-		//if it is going down AND left
-		else if (test_position[0] < test_position[2] && test_position[1] > test_position[3]) {
-			// when the left facing fish is off, turn it on
-			if (document.querySelector('.fish > img + img').classList.contains('display_n')) {
-				document.querySelector('.fish > img + img').classList.remove('display_n');
-				document.querySelector('.fish > img').classList.add('display_n');
-			}
-			else {
-				//do nothing
-			}
-
-			// turn the fish up
-			document.querySelector('.testing').classList.remove('right_forty_down');
-			document.querySelector('.testing').classList.add('right_forty_up');
-
-		}
-
-		else {
-
-			//its all good man
-		}
-
-
-		//document.querySelector('.fish > img:first').replaceWith('<img src="https://www.placecage.com/g/102/102" alt="" height="100" width="100">');
-
-	}
-
-
-	else {
-		//do nothing
-	}
-
-
-	// Adjusts the Z-INDEX on FISH
-	if (z_counter == 1) {
-		if (document.querySelector('.fish').classList.contains('z_4')) {
-			document.querySelector('.fish').classList.remove('z_4');
-			document.querySelector('.fish').classList.add('z_2');
-		}
-		else {
-			//do nothing
-		}
-	}
-	else {
-		if (document.querySelector('.fish').classList.contains('z_2')) {
-			document.querySelector('.fish').classList.remove('z_2');
-			document.querySelector('.fish').classList.add('z_4');
-		}
-		else {
-			//do nothing
-		}
-	}
-
-	// Keeps the COUNTER ARRAY from getting big
-	if (counter.reduce(getSum) > 1) {
-		counter.splice(2, 1);
-		//alert(test_position.valueOf());
-		test_position.splice(0, 2);
-	}
-	else {
-		//alert(test_position.valueOf());
-	}
-
-
-
-
-	$('.fish').animate({ top: newq[0], left: newq[1] }, newq[2], function () {
-
-		//alert(test_position.valueOf());
-		fishSwim();
-
-
-	});
-
-}
-
-
-/*function haveFish(){
-	$('.background').prepend('<img id="theImg" src="http://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg" />')
-};*/
-
-/*$('#btn1').click(function(){
-alert('Handler for .click() called.');
-	$('.background').append('<p>text</p>');
-}); */
-=======
 //WORK ON LATER IT'S TO ADD NEW FISH ELEMENTS PROPPER LOOK INTO DOCUMENT FRAGMENTS
 let addFish = function () {
 	const nufish = '<div class="fish z_4"> <img class="angle betta_fish" src="img/vector_fighting_fish_by_stu_artgraphics-d533k02.png" alt="" height="479" width="511">';
@@ -337,7 +129,6 @@ let fishSwim = function () {
 		}
 	});
 }
->>>>>>> 8fe96ab (Did a little more refactoring and commented out my code. Main things were: making makeNewPosition a IIEF and removed the portions that got the current postion into a new function getCurrentPosition incase I would ever want to call it more than once, Getting rid of the counter varible and the checking if counter had a number, don't know why I had that, and adjusting the if/else statments to have an else if; I don't know why it didn't do it like that before.)
 function changeBackground() {
 	var background_v = $('select[name="background"]').val();
 	var backgimg_1 = '<img class="responsive-img" src="img/water_sprite.svg" onerror="this.src= &quotimg/water_sprite.png &quot; this.onerror=null;" width="2000" height="550" alt="water sprite"/>';
@@ -442,8 +233,6 @@ function changeGravel() {
 	}
 }
 
-<<<<<<< HEAD
-=======
 //WORK ON LATER IT'S TO ADD NEW FISH ELEMENTS PROPPER LOOK INTO DOCUMENT FRAGMENTS
 /*function addFish(){
    const nufish = '<div class="fish z_4"> <img class="angle betta_fish" src="img/vector_fighting_fish_by_stu_artgraphics-d533k02.png" alt="" height="479" width="511">';
@@ -455,47 +244,9 @@ function changeGravel() {
 }
 */
 
->>>>>>> 8fe96ab (Did a little more refactoring and commented out my code. Main things were: making makeNewPosition a IIEF and removed the portions that got the current postion into a new function getCurrentPosition incase I would ever want to call it more than once, Getting rid of the counter varible and the checking if counter had a number, don't know why I had that, and adjusting the if/else statments to have an else if; I don't know why it didn't do it like that before.)
 $('#btn1').click(function () {
 	changeBackground();
 	changeMiddle();
 	changeFore();
 	changeGravel();
-
-});
-
-
-
-
-
-
-/*$(document).ready(function(){
-$("#btn1").click(function(){
-	var fish = $("#fish");
-	startAnimation();
-	function startAnimation(){
-	fish.animate({right: "300"});
-	fish.animate({bottom: "300"});
-	fish.animate({left: "0"});
-	fish.animate({top: "30"}, "slow", startAnimation);
-}
- });
-	});*/
-
-/*$(document).ready(function(){
-$("button").click(function(){
-	var div = $("div");
-	startAnimation();
-	function startAnimation(){
-		//div.animate({marginLeft: 300}, "slow");
-		div.animate({marginDown: 50}, "slow");
-		 
-	   /* div.animate({bottom: 100}, "slow");
-		div.animate({top: 100}, "slow", startAnimation);
-	}
-});
-});
-		*/
-/** functoin that moves image from spot 1 to spot 2 on button click.*/
-
-
+})
